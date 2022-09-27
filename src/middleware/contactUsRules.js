7011bbe3cb7c8b0d4a522026d4419e1bc786a880
HimawanArifSM <himawanarif7@gmail.com>
2022-09-27 13:16:34 +0700

    fix messages

--- src/middleware/contactUsRules.js
+++ src/middleware/contactUsRules.js
@@ -3,7 +3,7 @@ const {body} = require('express-validator');
 const contactUs = [
   body('fullname').isLength({min: 4}).withMessage('Fullname length minimal 4 character'),
   body('email').isEmail().withMessage('Email format invalid'),
-  body('message').isLength({max: 255}).withMessage('Phone number format is incorect'),
+  body('message').isLength({max: 255}).withMessage('Max text 255 character'),
 ];
 
 module.exports = contactUs;
